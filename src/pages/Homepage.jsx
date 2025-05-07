6ee6e13c6f84e5920c1eb26f462aa552ca8a871f
Suna <140689743+Ayyoh@users.noreply.github.com>
2025-05-07 21:55:05 +0800

    Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
     const {products, setProducts} = useProductStore();
 
     useEffect(() => {
-        fetch('/api/products')
+        fetch('https://vercel-backend-psi-ashy.vercel.app/products')
         .then(res => res.json())
         .then(json => setProducts(json))
     }, [])
